2ccb5db9c8e6d89f357d059132f259d838185e34
Eduardo Vieira <3413850@ifb.local>
2024-09-23 11:30:05 -0300

    Adicionando visualização do RGB para o target RGB

--- Assets/Testes/TargetSliderController.cs
+++ Assets/Testes/TargetSliderController.cs
@@ -4,35 +4,43 @@ using UnityEngine.UI;
 
 public class TargetSliderController : MonoBehaviour
 {
+    // Slidres
     public Slider redSlider;
     public Slider greenSlider;
     public Slider blueSlider;
 
+    // Textos dos sliders
     public Text redValueText;
     public Text greenValueText;
     public Text blueValueText;
+    public Text RGBValueText;
+    public Text RGBRedValueText;
+    public Text RGBGreenValueText;
+    public Text RGBBlueValueText;
 
+    // Objetos que representam as cores
     public GameObject redObject;
     public GameObject greenObject;
     public GameObject blueObject;
 
-    // Referência ao ImageTarget do Vuforia
+    // Targets de cor
     public GameObject redTarget;
     public GameObject greenTarget;
     public GameObject blueTarget;
+    public GameObject RGBTarget;
 
     private void Start()
     {
-        // Inicializa todos os sliders como invisíveis
+        // Inicializa todos os sliders e textos como invisíveis
         redSlider.gameObject.SetActive(false);
         greenSlider.gameObject.SetActive(false);
         blueSlider.gameObject.SetActive(false);
+        RGBValueText.gameObject.SetActive(false);
     }
 
     // Função chamada quando o target de cor vermelha é detectado
     public void OnRedTargetFound()
     {
-        Debug.Log("Red target found");
         redSlider.gameObject.SetActive(true);
         greenSlider.gameObject.SetActive(false);
         blueSlider.gameObject.SetActive(false);
@@ -41,7 +49,6 @@ public class TargetSliderController : MonoBehaviour
     // Função chamada quando o target de cor verde é detectado
     public void OnGreenTargetFound()
     {
-        Debug.Log("Green target found");
         redSlider.gameObject.SetActive(false);
         greenSlider.gameObject.SetActive(true);
         blueSlider.gameObject.SetActive(false);
@@ -50,10 +57,14 @@ public class TargetSliderController : MonoBehaviour
     // Função chamada quando o target de cor azul é detectado
     public void OnBlueTargetFound()
     {
-        Debug.Log("Blue target found");
         redSlider.gameObject.SetActive(false);
         greenSlider.gameObject.SetActive(false);
         blueSlider.gameObject.SetActive(true);
+    }
+
+    public void OnRGBTargetFound()
+    {
+        RGBValueText.gameObject.SetActive(true);
     }
 
     // Função chamada quando nenhum target é detectado
@@ -62,14 +73,19 @@ public class TargetSliderController : MonoBehaviour
         redSlider.gameObject.SetActive(false);
         greenSlider.gameObject.SetActive(false);
         blueSlider.gameObject.SetActive(false);
+        RGBValueText.gameObject.SetActive(false);
+
     }
 
     // Atualiza os textos dos sliders com os valores atuais (0-255)
     private void UpdateSliderValueTexts()
     {
         redValueText.text = ((int)redSlider.value).ToString();
+        RGBRedValueText.text = ((int)redSlider.value).ToString();
         greenValueText.text = ((int)greenSlider.value).ToString();
+        RGBGreenValueText.text = ((int)greenSlider.value).ToString();
         blueValueText.text = ((int)blueSlider.value).ToString();
+        RGBBlueValueText.text = ((int)blueSlider.value).ToString();
     }
 
     // Atualiza os textos quando os sliders são modificados
